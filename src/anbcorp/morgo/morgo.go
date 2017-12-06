c229ffaaa20a2f584e8289b0614d97265dc3ec14
Benoit 'Anb' Depail <bde@nbs-system.com>
2017-12-06 18:06:14 +0100

    Detecting win cases

--- src/anbcorp/morgo/morgo.go
+++ src/anbcorp/morgo/morgo.go
@@ -6,11 +6,22 @@ import (
     "time"
 )
 
+var board [9]int
+
+func randomBoard() {
+    rand.Seed(time.Now().UnixNano())
+    for i := 0; i<9; i+=1 {
+        board[i] = rand.Intn(2)
+    }
+}
+
 func initScreen() {
     var player int;
     var player_mark string;
 
-    rand.Seed(time.Now().UnixNano())
+    randomBoard()
+    fmt.Println(board)
+
     for j :=0; j<12;j+=1 {
         if j%4 == 0 {
             fmt.Println("+---+---+---+")
@@ -19,11 +30,11 @@ func initScreen() {
         } else {
             for i :=0; i<3; i+=1 {
                 fmt.Print("| ")
-                player = rand.Intn(2)
+                player = board[j/4*3+i]
                 if player == 0 {
-                    player_mark = "X"
-                } else {
                     player_mark = "O"
+                } else {
+                    player_mark = "X"
                 }
                 fmt.Print(player_mark)
                 fmt.Print(" ")
@@ -32,8 +43,56 @@ func initScreen() {
         }
     }
     fmt.Println("+---+---+---+")
+    winningBoard()
 }
 
+func sum(slice []int) (sum int) {
+    for _, v := range slice {
+        sum += v
+    }
+    return
+}
+func winningBoard() {
+    var set []int
+
+    for i :=0; i<3; i+=1 {
+        set = board[i*3:(i+1)*3]
+        if sum(set) == 0 {
+            fmt.Println("O wins on line", i)
+        }
+        if sum(set) == 3 {
+            fmt.Println("X wins on line", i)
+        }
+        fmt.Println(set, sum(set)==0, sum(set)==3)
+        set = nil
+    }
+    for i :=0; i<3; i+=1 {
+        set = append(set, board[0+i], board[3+i], board[6+i])
+        if sum(set) == 0 {
+            fmt.Println("O wins on column", i)
+        }
+        if sum(set) == 3 {
+            fmt.Println("X wins on column", i)
+        }
+        fmt.Println(set, sum(set)==0, sum(set)==3)
+        set = nil
+    }
+    // Testing diagonals
+    d1 := []int{board[0], board[4], board[8]}
+    d2 := []int{board[2], board[4], board[6]}
+    diags := [][]int{d1,d2}
+    for i, d := range diags {
+        fmt.Println("for", i, d)
+        if sum(d) == 0 {
+            fmt.Printf("O wins on diagonal %d,%d-%d,%d\n", 1+i,1+i,3-2*i,3-2*i)
+        }
+        if sum(d) == 3 {
+            fmt.Printf("X wins on diagonal %d,%d-%d,%d\n", 1+i,1+i,3-2*i,3-2*i)
+        }
+        fmt.Println(d, sum(d)==0, sum(d)==3)
+    }
+
+}
 func mainLoop() {
 }
 
